14ec34b0f3561c94b410416b16c8a4fad2564f7d
raniel150 <ranielbarles369@gmail.com>
2026-03-09 21:46:40 -0700

    Change database name from super_grocery_pos to pos_db

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn = mysqli_connect("localhost","root","","super_grocery_pos");
+$conn = mysqli_connect("localhost","root","","pos_db");
 if(!$conn){
  die("Connection failed: ".mysqli_connect_error());
 }
